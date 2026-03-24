e0b4f8acaa0796475d145dd05c4a4e7108a890c4
ZeyadMostafa22 <116260077+ZeyadMostafa22@users.noreply.github.com>
2026-03-24 19:08:09 +0200

    update question 13 - Time intervals between consecutive purchases for each customer

--- C-Customer-Behavior-Analysis/13 - Time intervals between consecutive purchases for each customer.sql
+++ C-Customer-Behavior-Analysis/13 - Time intervals between consecutive purchases for each customer.sql
@@ -1,4 +1,4 @@
--- Task 13: Measure time intervals between consecutive purchases for each customer.
+-- Task 13: Measure time intervals between consecutive purchases for each customer. [cite: 58]
 WITH order_dates AS (
     SELECT 
         f.customer_key,
@@ -6,18 +6,19 @@ WITH order_dates AS (
         f.order_id,
         d.full_date AS order_date 
     FROM fact_order_line f
-    JOIN dim_date     d ON f.date_key     = d.date_key
+    JOIN dim_date      d ON f.date_key      = d.date_key
     JOIN dim_customer c ON f.customer_key = c.customer_key
-    GROUP BY f.customer_key, c.customer_id, f.order_id
+    GROUP BY f.customer_key, c.customer_id, f.order_id, d.full_date 
 )
 SELECT 
     customer_id,
     order_id,
-    order_date                                                          AS current_purchase_date,
-    LAG(order_date) OVER (PARTITION BY customer_key ORDER BY order_date) AS prev_purchase_date,
+    order_date AS current_purchase_date,
+    LAG(order_date) OVER (PARTITION BY customer_id ORDER BY order_date) AS prev_purchase_date,
     DATEDIFF(
-        order_date,
-        LAG(order_date) OVER (PARTITION BY customer_key ORDER BY order_date)
-    )                                                                   AS days_between_purchases
+        'day', 
+        LAG(order_date) OVER (PARTITION BY customer_id ORDER BY order_date), 
+        order_date
+    ) AS days_between_purchases
 FROM order_dates
 ORDER BY customer_id, order_date;
